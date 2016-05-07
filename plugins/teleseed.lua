20b58288a99ce36196d5d1ab65a3472f16ccf633
sargardan <soheil22222222@users.noreply.github.com>
2016-05-07 19:38:29 +0430

    Update and rename blackPlus.lua to teleseed.lua

--- plugins/teleseed.lua
+++ plugins/teleseed.lua
@@ -121,7 +121,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "black" then -- Put everything you like :)
+    if matches[2] == "teleseed" then -- Put everything you like :)
       if not is_admin1(msg) then
         return "For admins only !"
       else
@@ -143,8 +143,8 @@ return {
     "^[#!/]([Ss]tats)$",
     "^[#!/]([Ss]tatslist)$",
     "^[#!/]([Ss]tats) (group) (%d+)",
-    "^[#!/]([Ss]tats) (black)",
-	"^[#!/]([Bb]lack)"
+    "^[#!/]([Ss]tats) (teleseed)",
+	"^[#!/]([Tt]eleseed)"
     }, 
   run = run
 }
