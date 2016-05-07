9f91d71a2adcd45c9ea1d1c63b8c03d3aee99a9e
sargardan <soheil22222222@users.noreply.github.com>
2016-05-07 19:16:58 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -1,175 +1,4 @@
-package.path = package.path .. ';.luarocks/share/lua/5.2/?.lua'
-  ..';.luarocks/share/lua/5.2/?/init.lua'
-package.cpath = package.cpath .. ';.luarocks/lib/lua/5.2/?.so'
-
-require("./bot/utils")
-
-local f = assert(io.popen('/usr/bin/git describe --tags', 'r'))
-VERSION = assert(f:read('*a'))
-f:close()
-
--- This function is called when tg receive a msg
-function on_msg_receive (msg)
-  if not started then
-    return
-  end
-
-  msg = backward_msg_format(msg)
-
-  local receiver = get_receiver(msg)
-  print(receiver)
-  --vardump(msg)
-  --vardump(msg)
-  msg = pre_process_service_msg(msg)
-  if msg_valid(msg) then
-    msg = pre_process_msg(msg)
-    if msg then
-      match_plugins(msg)
-      if redis:get("bot:markread") then
-        if redis:get("bot:markread") == "on" then
-          mark_read(receiver, ok_cb, false)
-        end
-      end
-    end
-  end
-end
-
-function ok_cb(extra, success, result)
-
-end
-
-function on_binlog_replay_end()
-  started = true
-  postpone (cron_plugins, false, 60*5.0)
-  -- See plugins/isup.lua as an example for cron
-
-  _config = load_config()
-
-  -- load plugins
-  plugins = {}
-  load_plugins()
-end
-
-function msg_valid(msg)
-  -- Don't process outgoing messages
-  if msg.out then
-    print('\27[36mNot valid: msg from us\27[39m')
-    return false
-  end
-
-  -- Before bot was started
-  if msg.date < os.time() - 5 then
-    print('\27[36mNot valid: old msg\27[39m')
-    return false
-  end
-
-  if msg.unread == 0 then
-    print('\27[36mNot valid: readed\27[39m')
-    return false
-  end
-
-  if not msg.to.id then
-    print('\27[36mNot valid: To id not provided\27[39m')
-    return false
-  end
-
-  if not msg.from.id then
-    print('\27[36mNot valid: From id not provided\27[39m')
-    return false
-  end
-
-  if msg.from.id == our_id then
-    print('\27[36mNot valid: Msg from our id\27[39m')
-    return false
-  end
-
-  if msg.to.type == 'encr_chat' then
-    print('\27[36mNot valid: Encrypted chat\27[39m')
-    return false
-  end
-
-  if msg.from.id == 777000 then
-    --send_large_msg(*group id*, msg.text) *login code will be sent to GroupID*
-    return false
-  end
-
-  return true
-end
-
---
-function pre_process_service_msg(msg)
-   if msg.service then
-      local action = msg.action or {type=""}
-      -- Double ! to discriminate of normal actions
-      msg.text = "!!tgservice " .. action.type
-
-      -- wipe the data to allow the bot to read service messages
-      if msg.out then
-         msg.out = false
-      end
-      if msg.from.id == our_id then
-         msg.from.id = 0
-      end
-   end
-   return msg
-end
-
--- Apply plugin.pre_process function
-function pre_process_msg(msg)
-  for name,plugin in pairs(plugins) do
-    if plugin.pre_process and msg then
-      print('Preprocess', name)
-      msg = plugin.pre_process(msg)
-    end
-  end
-  return msg
-end
-
--- Go over enabled plugins patterns.
-function match_plugins(msg)
-  for name, plugin in pairs(plugins) do
-    match_plugin(plugin, name, msg)
-  end
-end
-
--- Check if plugin is on _config.disabled_plugin_on_chat table
-local function is_plugin_disabled_on_chat(plugin_name, receiver)
-  local disabled_chats = _config.disabled_plugin_on_chat
-  -- Table exists and chat has disabled plugins
-  if disabled_chats and disabled_chats[receiver] then
-    -- Checks if plugin is disabled on this chat
-    for disabled_plugin,disabled in pairs(disabled_chats[receiver]) do
-      if disabled_plugin == plugin_name and disabled then
-        local warning = 'Plugin '..disabled_plugin..' is disabled on this chat'
-        print(warning)
-        send_msg(receiver, warning, ok_cb, false)
-        return true
-      end
-    end
-  end
-  return false
-end
-
-function match_plugin(plugin, plugin_name, msg)
-  local receiver = get_receiver(msg)
-
-  -- Go over patterns. If one matches it's enough.
-  for k, pattern in pairs(plugin.patterns) do
-    local matches = match_pattern(pattern, msg.text)
-    if matches then
-      print("msg matches: ", pattern)
-
-      if is_plugin_disabled_on_chat(plugin_name, receiver) then
-        return nil
-      end
-      -- Function exists
-      if plugin.run then
-        -- If plugin is for privileged users only
-        if not warns_user_not_allowed(plugin, msg) then
-          local result = plugin.run(msg, matches)
-          if result then
-            send_large_msg(receiver, result)
-          end
+package.path = package.path .. ';.luarocks/s     end
         end
       end
       -- One patterns matches
@@ -259,58 +88,83 @@ function create_config( )
     "inAdmin"
 
     },
-    sudo_users = {56693692},--Sudo users
+    sudo_users = {192281402},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[ ]],
     help_text_realm = [[
-Realm Commands:
-#creategroup [Name]
-Create a group
-#createrealm [Name]
-Create a realm
-#setname [Name]
-Set realm name
-#setabout [group|sgroup] [GroupID] [Text]
-Set a group's about text
-#setrules [GroupID] [Text]
-Set a group's rules
-#lock [GroupID] [setting]
-Lock a group's setting
-#unlock [GroupID] [setting]
-Unock a group's setting
-#settings [group|sgroup] [GroupID]
-Set settings for GroupID
-#wholist
-Get a list of members in group/realm
-#who
-Get a file of members in group/realm
-#type
-Get group type
-#addadmin [id|username]
-Promote an admin by id OR username *Sudo only
-#removeadmin [id|username]
-Demote an admin by id OR username *Sudo only
-#list groups
-Get a list of all groups
-#list realms
-Get a list of all realms
-#support
-Promote user to support
-#-support
-Demote user from support
-#log
-Get a logfile of current group or realm
-#broadcast [text]
-#broadcast Hello !
-Send text to all groups
-Only sudo users can run this command
-#bc [group_id] [text]
-#bc 123456789 Hello !
-This command will send text to [group_id]
-**You can use "#", "!", or "/" to begin all commands
-*Only admins and sudo can add bots in group
-*Only admins and sudo can use kick,ban,unban,newlink,setphoto,setname,lock,unlock,set rules,set about and settings commands
-*Only admins and sudo can use res, setowner, commands
+📥Realm Commands📤
+
+︿﹀︿﹀︿﹀︿﹀︿﹀︿﹀
+💎 /creategroup [نام]
+
+گروه جدید بسازید
+
+💎 /createrealm [نام]
+گروه اصلی بسازید
+ 
+💎 /setname [اسم]
+اسم گروه اصلی را تغییربدهید
+
+💎 /setabout [GroupId] [Text]
+درمورد ان گروه توضیحاتی را بنویسید (ایدی گروه را بدهید)
+
+💎 /setrules [GroupID] [Text]
+درمورد ان گروه قوانینی تعیین کنید (ایدی گروه رابدهید)
+
+💎 /lock [GroupID] [setting]
+تنظیمات گروهی را قفل بکنید
+
+💎 /unlock [GroupID] [setting]
+تنظیمات گروهی را ازقفل در بیاورید
+
+💎 /settings[GroupID]
+تنظیمات گروه را تغییر بدهید 
+
+💎 /wholist
+لیست تمامی اعضای گروه را با ایدی نشان خواهد داد
+
+💎 /who
+لیست تمامی اعضای گروه را باایدی نشان خواهد داد
+
+💎 /type
+درمورد نقش گروه بگیرید
+
+💎 /kill chat [GroupID]
+تمامی اعضای گروه را حذف میکنید
+
+💎 /kill realm [RealmID]
+تمامی اعضای گروه مادر را حذف میکند
+
+💎 /addadmin [id|username]
+اضافه شدن به ادمین بات
+
+💎 /removeadmin [id|username]
+حذف از ادمینی بات 
+
+💎 /list groups
+لیست گروه های بات
+
+💎 /list realms
+لیست گروه های اصلی بات
+
+💎 /support
+اضافه شدن به ساپورت بات (ترفیع)
+
+💎 /-support
+حذف شدن از ساپورت بات 
+(تنزل)
+💎 /log
+تمامی عملیات گروه رامیدهد
+
+💎 /broadcast [text]
+فرستادن پیام به تمامی گروه های بات
+
+میتوانید از هردوی کاراکتر های { /و ! } برای دستورات استفاده کنید
+
+︿﹀︿﹀︿﹀︿﹀︿﹀︿﹀
+
+Channel: @teleirans
+
 ]],
     help_text = [[
 Commands list :
